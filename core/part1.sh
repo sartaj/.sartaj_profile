3253a7540f330fda263d54e4bbfb70919dbd5968
Sartaj <sartaj@sartaj.me>
2019-09-24 05:39:05 -0500

    Update part1.sh

--- core/part1.sh
+++ core/part1.sh
@@ -7,4 +7,4 @@ source ./download-cli-package-managers.sh
 echo 'First Install Complete. Please Do The Following'
 echo 'sudo xcodebuild -license accept'
 echo 'mas login --prompt EMAIL'
-echo 'open a  new window and run "source setup_part2.sh"'
+echo 'open a  new window and run "source ~/.sartaj_profile/setup/part2.sh"'
